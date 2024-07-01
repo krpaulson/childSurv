73af25f8fcc920ca6afc669a8a158b5d99ba2947
Katie Paulson <krpaulson@gmail.com>
2024-07-01 10:31:34 -0700

    add option to include IID effect in prediction

--- R/log_logistic.R
+++ R/log_logistic.R
@@ -1,17 +1,19 @@
 
 #' Fit log-logistic model
 #'
-#' @param data_vr input data
+#' @param data_vr input data counts
+#' @param data_direct input data in pre-processed form
 #' @param time_model spline or rw2
 #' @param start_year first year to estimate
 #' @param end_year last year to estimate
+#' @param include_iid_in_pred include IID errors in prediction
 #'
 #' @return
 #' @export
 #' @importFrom stats nlminb
 #'
 #' @examples
-log_logistic <- function(data_vr, time_model, start_year = 1950, end_year = 2030) {
+log_logistic <- function(data_vr, data_direct, time_model, start_year = 1950, end_year = 2030, include_iid_in_pred = T) {
   # add: data_fbh, data_other
   
   n_years <- length(start_year:end_year)
@@ -55,19 +57,27 @@ log_logistic <- function(data_vr, time_model, start_year = 1950, end_year = 2030
                       obs_vr = data_vr$obs_vr,
                       n_vr = data_vr$n_vr,
                       births = data_vr$births,
-                      pop = data_vr$pop)
-    param_list <- list(intercept_log_shape = -1,
-                       intercept_log_scale = 15,
+                      pop = data_vr$pop) #,
+                      # n_obs_direct = data_direct$n_obs_direct,
+                      # time_id_direct = data_direct$time_id_direct,
+                      # months_direct = data_direct$months_direct,
+                      # obs_direct = data_direct$obs_direct,
+                      # se_direct = data_direct$se_direct)
+    param_list <- list(intercept_log_shape = 0.7, #-1.5,
+                       intercept_log_scale = 39,
                        log_tau_delta_log_shape = 0,
                        log_tau_delta_log_scale = 0,
-                       log_tau_epsilon_log_shape = 0,
-                       log_tau_epsilon_log_scale = 0,
+                       # log_tau_epsilon_log_shape = 0,
+                       # log_tau_epsilon_log_scale = 0,
+                       # epsilon = rep(0, data_vr$n_obs_vr),
+                       # log_prec_epsilon = 9,
+                       log_phi = 0,
                        delta_log_shape = rep(0, n_years),
-                       delta_log_scale = rep(0, n_years),
-                       epsilon_log_shape = rep(0, n_years),
-                       epsilon_log_scale = rep(0, n_years))
-    random = c("delta_log_shape", "delta_log_scale",
-               "epsilon_log_shape", "epsilon_log_scale")
+                       delta_log_scale = rep(0, n_years)) #,
+                       #epsilon_log_shape = rep(0, n_years),
+                       #epsilon_log_scale = rep(0, n_years))
+    random = c("delta_log_shape", "delta_log_scale") #,
+               #"epsilon_log_shape", "epsilon_log_scale")
       
   # } else {
   #   stop("Invalid time model.")
@@ -81,8 +91,8 @@ log_logistic <- function(data_vr, time_model, start_year = 1950, end_year = 2030
                         DLL = "childSurvLL_TMBExports")
   
   opt <- nlminb(obj$par, obj$fn, obj$gr,
-                lower     = c(-60,0, rep(-60,4)),
-                upper     = c(0,600, rep(100,4)))
+                lower = c(-1, 2, -10, -15, -2), #, -10, -10, -2), #, rep(-1, data_vr$n_obs_vr), 3),  # log_shape -5 to -0.2
+                upper = c(2, 250, 10, 10, 4)) #20, 20, 4)) #, rep(1, data_vr$n_obs_vr), 15)) # log-log-shape -2 to 2
   SD0 <- TMB::sdreport(obj,
                        getJointPrecision = TRUE,
                        getReportCovariance = TRUE,
@@ -99,6 +109,8 @@ log_logistic <- function(data_vr, time_model, start_year = 1950, end_year = 2030
   intercept.scale.idx <- which(names(mu) == "intercept_log_scale")
   time.struct.shape.idx <- which(names(mu) == "delta_log_shape")
   time.struct.scale.idx <- which(names(mu) == "delta_log_scale")
+  time.unstruct.shape.idx <- which(names(mu) == "epsilon_log_shape")
+  time.unstruct.scale.idx <- which(names(mu) == "epsilon_log_scale")
   
   if (time_model == "spline") {
     
@@ -155,14 +167,18 @@ log_logistic <- function(data_vr, time_model, start_year = 1950, end_year = 2030
     # combine draws for linear predictor (log shape)
     fitted_shape <-
       matrix(rep(t.draws[intercept.shape.idx,], n_years), nrow = n_years, byrow = T) +
-      #t.draws[time.unstruct.shape.idx,] +
       t.draws[time.struct.shape.idx,]
     
     # combine draws for linear predictor (log scale)
     fitted_scale <-
       matrix(rep(t.draws[intercept.scale.idx,], n_years), nrow = n_years, byrow = T) +
-      #t.draws[time.unstruct.scale.idx,] +
       t.draws[time.struct.scale.idx,]
+    
+    # add IID
+    if (include_iid_in_pred) {
+      fitted_shape <- fitted_shape + t.draws[time.unstruct.shape.idx,]
+      fitted_scale <- fitted_scale + t.draws[time.unstruct.scale.idx,]
+    }
     
   } else {
     stop("Invalid temporal model.")
@@ -173,9 +189,9 @@ log_logistic <- function(data_vr, time_model, start_year = 1950, end_year = 2030
   imr_mat <- matrix(nrow = n_years, ncol = 1000)
   u5mr_mat <- matrix(nrow = n_years, ncol = 1000)
   for (t in 1:n_years) {
-    nmr_mat[t,] <- flexsurv::pllogis(1, exp(fitted_shape)[t,], exp(fitted_scale)[t,])
-    imr_mat[t,] <- flexsurv::pllogis(12, exp(fitted_shape)[t,], exp(fitted_scale)[t,])
-    u5mr_mat[t,] <- flexsurv::pllogis(60, exp(fitted_shape)[t,], exp(fitted_scale)[t,])
+    nmr_mat[t,] <- flexsurv::pllogis(1, exp(-1*exp(fitted_shape))[t,], exp(fitted_scale)[t,])
+    imr_mat[t,] <- flexsurv::pllogis(12, exp(-1*exp(fitted_shape))[t,], exp(fitted_scale)[t,])
+    u5mr_mat[t,] <- flexsurv::pllogis(60, exp(-1*exp(fitted_shape))[t,], exp(fitted_scale)[t,])
   }
   
   #  create prediction data.frame
